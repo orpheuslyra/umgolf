d0ad45531246a7aa066fc8d0e163fd0f0ca438f3
orpheuslyra <proserpine0922@gmail.com>
2025-11-19 22:49:17 +0900

    ファーストコミット

---
+++ src/assets/images.ts
@@ -0,0 +1,50 @@
+// 画像アセットのimport定義
+import heroImage from './images/keyvisual.png';
+import heroImageSp from './images/keyvisual_sp.png';
+import golfIcon1 from './images/golf-icon-1.png';
+import golfIcon2 from './images/golf-icon-2.png';
+import golfIcon3 from './images/golf-icon-3.png';
+import golfIcon6 from './images/golf-icon-6.png';
+import coach1 from './images/tanaka.png';
+import coach2 from './images/nishi.jpg';
+import coach3 from './images/soga.png';
+import simulator from './images/simulator.png';
+import proV1 from './images/golfball.png';
+import practice from './images/practice.JPG';
+import golfer1 from './images/golfer1.png';
+import golfer2 from './images/golfer2.png';
+import golfer3 from './images/golfer3.png';
+import golfer4 from './images/golfer4.png';
+import mailIcon from './images/mail.png';
+import wwwIcon from './images/www.png';
+import golfProblems from './images/problems.png';
+import umFeature from './images/umgolf_feature.png';
+import umgolfResult from './images/result.png';
+import plan from './images/plan.png';
+import UMLogo from './images/UMLogo.png';
+
+export const images = {
+  hero: heroImage,
+  heroSp: heroImageSp,
+  golfIcon1,
+  golfIcon2,
+  golfIcon3,
+  golfIcon6,
+  coach1,
+  coach2,
+  coach3,
+  simulator,
+  proV1,
+  practice,
+  golfer1,
+  golfer2,
+  golfer3,
+  golfer4,
+  mailIcon,
+  wwwIcon,
+  golfProblems,
+  umFeature,
+  umgolfResult,
+  plan,
+  UMLogo
+} as const;
